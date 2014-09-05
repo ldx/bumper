067dc7e32b3ef8df98018f4349d378bb967ec7fb
Vilmos Nebehaj <v.nebehaj@gmail.com>
2014-09-06 00:21:19 +0200

    Use new gofmt.

--- main.go
+++ main.go
@@ -1,223 +1,223 @@
 package main
 
 import (
-    "bufio"
-    "crypto/tls"
-    "fmt"
-    "github.com/jessevdk/go-flags"
-    "io"
-    "log"
-    "net"
-    "net/http"
-    "net/url"
-    "os"
-    "strings"
-    "sync"
+	"bufio"
+	"crypto/tls"
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"sync"
 )
 
 //
 // This holds all the certificate information for Bumper.
 //
 type BumperProxy struct {
-    cacert     tls.Certificate
-    certs      map[string]*tls.Certificate
-    mutex      *sync.RWMutex
-    certdir    string
-    maxserial  int64
-    proxy      string
-    skipverify bool
-    addorig    bool
+	cacert     tls.Certificate
+	certs      map[string]*tls.Certificate
+	mutex      *sync.RWMutex
+	certdir    string
+	maxserial  int64
+	proxy      string
+	skipverify bool
+	addorig    bool
 }
 
 //
 // Main listener loop.
 //
 func Loop(addr string, bumper *BumperProxy) {
-    log.Printf("Starting listener on %s\n", addr)
+	log.Printf("Starting listener on %s\n", addr)
 
-    srv, err := net.Listen("tcp", addr)
-    if err != nil {
-        log.Printf("Can't start listener on %s: %s\n", addr, err)
-        os.Exit(1)
-    }
+	srv, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Printf("Can't start listener on %s: %s\n", addr, err)
+		os.Exit(1)
+	}
 
-    for {
-        conn, err := srv.Accept()
-        if err != nil {
-            log.Printf("Warning: accepting client: %s\n", err)
-            continue
-        }
+	for {
+		conn, err := srv.Accept()
+		if err != nil {
+			log.Printf("Warning: accepting client: %s\n", err)
+			continue
+		}
 
-        go HandleClient(conn, bumper)
-    }
+		go HandleClient(conn, bumper)
+	}
 }
 
 //
 // Goroutine handling a client, proxying requests and responses.
 //
 func HandleClient(conn net.Conn, bumper *BumperProxy) {
-    defer conn.Close()
+	defer conn.Close()
 
-    var proxy func(*http.Request) (*url.URL, error) = nil
-    if bumper.proxy != "" {
-        proxyurl, err := url.Parse("http://" + bumper.proxy)
-        if err != nil {
-            log.Printf("Setting parent proxy to %s: %s\n",
-                bumper.proxy, err)
-            return
-        }
-        proxy = http.ProxyURL(proxyurl)
-    }
+	var proxy func(*http.Request) (*url.URL, error) = nil
+	if bumper.proxy != "" {
+		proxyurl, err := url.Parse("http://" + bumper.proxy)
+		if err != nil {
+			log.Printf("Setting parent proxy to %s: %s\n",
+				bumper.proxy, err)
+			return
+		}
+		proxy = http.ProxyURL(proxyurl)
+	}
 
-    tr := &http.Transport{
-        Proxy: proxy,
-        TLSClientConfig: &tls.Config{
-            InsecureSkipVerify: bumper.skipverify,
-        },
-    }
+	tr := &http.Transport{
+		Proxy: proxy,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: bumper.skipverify,
+		},
+	}
 
-    cli := conn.RemoteAddr().String()
+	cli := conn.RemoteAddr().String()
 
-    reader := bufio.NewReader(conn)
-    writer := io.Writer(conn)
+	reader := bufio.NewReader(conn)
+	writer := io.Writer(conn)
 
-    orig_uri := ""
-    for {
-        req, err := http.ReadRequest(reader)
-        if err == io.EOF {
-            log.Printf("(%s) client closed connection\n", cli)
-            return
-        } else if err != nil {
-            log.Printf("(%s) error reading request: %s\n", cli, err)
-            return
-        }
+	orig_uri := ""
+	for {
+		req, err := http.ReadRequest(reader)
+		if err == io.EOF {
+			log.Printf("(%s) client closed connection\n", cli)
+			return
+		} else if err != nil {
+			log.Printf("(%s) error reading request: %s\n", cli, err)
+			return
+		}
 
-        log.Printf("(%s) -> %s %s\n", cli, req.Method, req.RequestURI)
-        //req.Write(os.Stdout)
+		log.Printf("(%s) -> %s %s\n", cli, req.Method, req.RequestURI)
+		//req.Write(os.Stdout)
 
-        if req.Method == "CONNECT" {
-            if !strings.Contains(req.Host, ":") {
-                SendResp(cli, writer, 400,
-                    fmt.Sprintf("invalid host '%s'", req.Host), false)
-                return
-            }
-            host := strings.Split(req.Host, ":")[0]
+		if req.Method == "CONNECT" {
+			if !strings.Contains(req.Host, ":") {
+				SendResp(cli, writer, 400,
+					fmt.Sprintf("invalid host '%s'", req.Host), false)
+				return
+			}
+			host := strings.Split(req.Host, ":")[0]
 
-            // Retrieve or create fake certificate for 'host'.
-            cert, err := GetCertificate(host, bumper)
-            if err != nil {
-                log.Printf("(%s) error getting new cert: %s\n", cli, err)
-                SendResp(cli, writer, 500, err.Error(), false)
-                return
-            }
+			// Retrieve or create fake certificate for 'host'.
+			cert, err := GetCertificate(host, bumper)
+			if err != nil {
+				log.Printf("(%s) error getting new cert: %s\n", cli, err)
+				SendResp(cli, writer, 500, err.Error(), false)
+				return
+			}
 
-            // Okay, we are ready to start TLS.
-            SendResp(cli, writer, 200, "", true)
+			// Okay, we are ready to start TLS.
+			SendResp(cli, writer, 200, "", true)
 
-            tlsconn, err := StartTls(conn, cert)
-            if err != nil {
-                log.Printf("(%s) error starting TLS: %s\n", cli, err)
-                return
-            }
-            defer tlsconn.Close()
+			tlsconn, err := StartTls(conn, cert)
+			if err != nil {
+				log.Printf("(%s) error starting TLS: %s\n", cli, err)
+				return
+			}
+			defer tlsconn.Close()
 
-            reader = bufio.NewReader(tlsconn)
-            writer = io.Writer(tlsconn)
+			reader = bufio.NewReader(tlsconn)
+			writer = io.Writer(tlsconn)
 
-            orig_uri = req.RequestURI
+			orig_uri = req.RequestURI
 
-            continue
-        }
+			continue
+		}
 
-        if FixRequest(req, orig_uri, bumper.addorig) != nil {
-            log.Printf("(%s) invalid request URI %s\n", cli,
-                req.RequestURI)
-            return
-        }
+		if FixRequest(req, orig_uri, bumper.addorig) != nil {
+			log.Printf("(%s) invalid request URI %s\n", cli,
+				req.RequestURI)
+			return
+		}
 
-        resp, err := tr.RoundTrip(req)
-        if err != nil {
-            statuscode := 502
-            if resp != nil {
-                statuscode = resp.StatusCode
-            }
-            SendResp(cli, writer, statuscode, err.Error(), false)
-            return
-        }
+		resp, err := tr.RoundTrip(req)
+		if err != nil {
+			statuscode := 502
+			if resp != nil {
+				statuscode = resp.StatusCode
+			}
+			SendResp(cli, writer, statuscode, err.Error(), false)
+			return
+		}
 
-        if FixResponse(resp) != nil {
-            log.Printf("(%s) error fixing Content-Lenght for %s: %s\n",
-                cli, req, err)
-            SendResp(cli, writer, 500, err.Error(), false)
-            return
-        }
+		if FixResponse(resp) != nil {
+			log.Printf("(%s) error fixing Content-Lenght for %s: %s\n",
+				cli, req, err)
+			SendResp(cli, writer, 500, err.Error(), false)
+			return
+		}
 
-        resp.Write(writer)
+		resp.Write(writer)
 
-        log.Printf("(%s) <- %s %s\n", cli, resp.Status, req.URL)
-        //resp.Write(os.Stdout)
+		log.Printf("(%s) <- %s %s\n", cli, resp.Status, req.URL)
+		//resp.Write(os.Stdout)
 
-        if req.Close || resp.Close {
-            log.Printf("(%s) closing connection\n", cli)
-            return
-        }
-    }
+		if req.Close || resp.Close {
+			log.Printf("(%s) closing connection\n", cli)
+			return
+		}
+	}
 }
 
 //
 // Command line options.
 //
 var opts struct {
-    CertDir     string `short:"d" long:"certdir" value-name:"<directory>" description:"Directory where generated certificates are stored." required:"true"`
-    CaCert      string `short:"c" long:"cacert" value-name:"<file>" description:"CA certificate file." required:"true"`
-    CaKey       string `short:"k" long:"cakey" value-name:"<file>" description:"CA private key file." required:"true"`
-    Proxy       string `short:"p" long:"proxy" value-name:"<host:port>" description:"HTTP parent proxy to use for all requests (both HTTP and HTTPS)."`
-    SkipVerify  bool   `short:"n" long:"skipverify" description:"If set, BumperProxy will not verify the certificate of HTTPS websites." default:"false"`
-    Listen      string `short:"l" long:"listen" value-name:"<host:port>" description:"Host and port where Bumperproxy will be listening." default:"localhost:9718"`
-    AddXOrigUri bool   `short:"x" long:"addxoriguri" description:"If set, BumperProxy will add an X-Orig-Uri header with the original URI to requests." default:"false"`
-    Verbose     []bool `short:"v" long:"verbose" description:"Enable verbose debugging."`
+	CertDir     string `short:"d" long:"certdir" value-name:"<directory>" description:"Directory where generated certificates are stored." required:"true"`
+	CaCert      string `short:"c" long:"cacert" value-name:"<file>" description:"CA certificate file." required:"true"`
+	CaKey       string `short:"k" long:"cakey" value-name:"<file>" description:"CA private key file." required:"true"`
+	Proxy       string `short:"p" long:"proxy" value-name:"<host:port>" description:"HTTP parent proxy to use for all requests (both HTTP and HTTPS)."`
+	SkipVerify  bool   `short:"n" long:"skipverify" description:"If set, BumperProxy will not verify the certificate of HTTPS websites." default:"false"`
+	Listen      string `short:"l" long:"listen" value-name:"<host:port>" description:"Host and port where Bumperproxy will be listening." default:"localhost:9718"`
+	AddXOrigUri bool   `short:"x" long:"addxoriguri" description:"If set, BumperProxy will add an X-Orig-Uri header with the original URI to requests." default:"false"`
+	Verbose     []bool `short:"v" long:"verbose" description:"Enable verbose debugging."`
 }
 
 func main() {
-    args, err := flags.Parse(&opts)
-    if err != nil {
-        if len(args) == 1 && args[0] == "--help" {
-            os.Exit(0)
-        } else {
-            os.Exit(1)
-        }
-    }
+	args, err := flags.Parse(&opts)
+	if err != nil {
+		if len(args) == 1 && args[0] == "--help" {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
+	}
 
-    //log.SetOutput(os.Stdout)
-    log.SetPrefix("[bumper] ")
+	//log.SetOutput(os.Stdout)
+	log.SetPrefix("[bumper] ")
 
-    bumper := new(BumperProxy)
+	bumper := new(BumperProxy)
 
-    bumper.proxy = opts.Proxy
-    bumper.addorig = opts.AddXOrigUri
-    bumper.skipverify = opts.SkipVerify
+	bumper.proxy = opts.Proxy
+	bumper.addorig = opts.AddXOrigUri
+	bumper.skipverify = opts.SkipVerify
 
-    // Load CA certificate and key.
-    cacert, err := ReadCert(opts.CaCert, opts.CaKey)
-    if err != nil {
-        log.Printf("%s\n", err)
-        os.Exit(1)
-    }
-    bumper.cacert = *cacert
+	// Load CA certificate and key.
+	cacert, err := ReadCert(opts.CaCert, opts.CaKey)
+	if err != nil {
+		log.Printf("%s\n", err)
+		os.Exit(1)
+	}
+	bumper.cacert = *cacert
 
-    // Load server certificates.
-    bumper.certdir = opts.CertDir
+	// Load server certificates.
+	bumper.certdir = opts.CertDir
 
-    bumper.certs = make(map[string]*tls.Certificate)
-    err = ReadCertificates(bumper.certdir, &bumper.cacert, bumper.certs,
-        &bumper.maxserial)
-    if err != nil {
-        log.Printf("Error loading certificates from '%s': %s\n", opts.CertDir,
-            err)
-        os.Exit(1)
-    }
+	bumper.certs = make(map[string]*tls.Certificate)
+	err = ReadCertificates(bumper.certdir, &bumper.cacert, bumper.certs,
+		&bumper.maxserial)
+	if err != nil {
+		log.Printf("Error loading certificates from '%s': %s\n", opts.CertDir,
+			err)
+		os.Exit(1)
+	}
 
-    bumper.mutex = new(sync.RWMutex)
+	bumper.mutex = new(sync.RWMutex)
 
-    Loop(opts.Listen, bumper)
+	Loop(opts.Listen, bumper)
 }
--- tlsutil.go
+++ tlsutil.go
@@ -1,247 +1,247 @@
 package main
 
 import (
-    "crypto/rand"
-    "crypto/rsa"
-    _ "crypto/sha512"
-    "crypto/tls"
-    "crypto/x509"
-    "crypto/x509/pkix"
-    "encoding/pem"
-    "errors"
-    "fmt"
-    "log"
-    "math/big"
-    "net"
-    "os"
-    "strings"
-    "time"
+	"crypto/rand"
+	"crypto/rsa"
+	_ "crypto/sha512"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"log"
+	"math/big"
+	"net"
+	"os"
+	"strings"
+	"time"
 )
 
 func StartTls(conn net.Conn, cert *tls.Certificate) (tlsconn *tls.Conn,
-    err error) {
-    config := &tls.Config{
-        NextProtos:   []string{"HTTP/1.1"},
-        Certificates: []tls.Certificate{*cert},
-    }
+	err error) {
+	config := &tls.Config{
+		NextProtos:   []string{"HTTP/1.1"},
+		Certificates: []tls.Certificate{*cert},
+	}
 
-    tlsconn = tls.Server(conn, config)
-    tlsconn.Handshake()
+	tlsconn = tls.Server(conn, config)
+	tlsconn.Handshake()
 
-    return tlsconn, nil
+	return tlsconn, nil
 }
 
 func GetCertificate(name string, bumper *BumperProxy) (cert *tls.Certificate,
-    err error) {
-    // Check if we have the certificate for the server in our map.
-    bumper.mutex.RLock()
-    cert, ok := bumper.certs[name]
-    bumper.mutex.RUnlock()
-    if ok {
-        return cert, nil
-    }
+	err error) {
+	// Check if we have the certificate for the server in our map.
+	bumper.mutex.RLock()
+	cert, ok := bumper.certs[name]
+	bumper.mutex.RUnlock()
+	if ok {
+		return cert, nil
+	}
 
-    // We have to create the certificate.
-    key, err := rsa.GenerateKey(rand.Reader, 1024)
-    if err != nil {
-        return nil, errors.New("Certificate generation failed")
-    }
-    privkey := x509.MarshalPKCS1PrivateKey(key)
+	// We have to create the certificate.
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		return nil, errors.New("Certificate generation failed")
+	}
+	privkey := x509.MarshalPKCS1PrivateKey(key)
 
-    bumper.mutex.Lock()
-    bumper.maxserial++
-    serial := bumper.maxserial
-    bumper.mutex.Unlock()
+	bumper.mutex.Lock()
+	bumper.maxserial++
+	serial := bumper.maxserial
+	bumper.mutex.Unlock()
 
-    var ipaddrs = []net.IP{}
-    ipaddr := net.ParseIP(name)
-    if ipaddr != nil {
-        ipaddrs = []net.IP{ipaddr}
-    }
+	var ipaddrs = []net.IP{}
+	ipaddr := net.ParseIP(name)
+	if ipaddr != nil {
+		ipaddrs = []net.IP{ipaddr}
+	}
 
-    dercert, err := x509.CreateCertificate(
-        rand.Reader,
-        &x509.Certificate{
-            Subject: pkix.Name{
-                CommonName:   name,
-                Organization: []string{"Bumper Proxy LLC"},
-            },
-            KeyUsage: (x509.KeyUsageDigitalSignature |
-                x509.KeyUsageKeyEncipherment),
-            SerialNumber: big.NewInt(serial),
-            NotAfter:     time.Now().AddDate(10, 0, 0).UTC(),
-            NotBefore:    time.Now().AddDate(-10, 0, 0).UTC(),
-            IPAddresses:  ipaddrs,
-        },
-        bumper.cacert.Leaf,
-        &key.PublicKey,
-        bumper.cacert.PrivateKey)
-    if err != nil {
-        return nil, errors.New("Certificate generation failed")
-    }
+	dercert, err := x509.CreateCertificate(
+		rand.Reader,
+		&x509.Certificate{
+			Subject: pkix.Name{
+				CommonName:   name,
+				Organization: []string{"Bumper Proxy LLC"},
+			},
+			KeyUsage: (x509.KeyUsageDigitalSignature |
+				x509.KeyUsageKeyEncipherment),
+			SerialNumber: big.NewInt(serial),
+			NotAfter:     time.Now().AddDate(10, 0, 0).UTC(),
+			NotBefore:    time.Now().AddDate(-10, 0, 0).UTC(),
+			IPAddresses:  ipaddrs,
+		},
+		bumper.cacert.Leaf,
+		&key.PublicKey,
+		bumper.cacert.PrivateKey)
+	if err != nil {
+		return nil, errors.New("Certificate generation failed")
+	}
 
-    certblock := &pem.Block{
-        Type:  "CERTIFICATE",
-        Bytes: dercert,
-    }
-    keyblock := &pem.Block{
-        Type:  "RSA PRIVATE KEY",
-        Bytes: privkey,
-    }
+	certblock := &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: dercert,
+	}
+	keyblock := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privkey,
+	}
 
-    pemcert := pem.EncodeToMemory(certblock)
-    pemkey := pem.EncodeToMemory(keyblock)
+	pemcert := pem.EncodeToMemory(certblock)
+	pemkey := pem.EncodeToMemory(keyblock)
 
-    crt, err := tls.X509KeyPair(pemcert, pemkey)
-    if err != nil {
-        return nil, errors.New("Certificate generation failed")
-    }
-    cert = &crt
+	crt, err := tls.X509KeyPair(pemcert, pemkey)
+	if err != nil {
+		return nil, errors.New("Certificate generation failed")
+	}
+	cert = &crt
 
-    path := fmt.Sprintf("%s%c%s.crt", bumper.certdir, os.PathSeparator, name)
-    certfile, err := os.Create(path)
-    if err != nil {
-        return nil, errors.New(
-            fmt.Sprintf("Failed to save certificate as %s", path))
-    }
-    pem.Encode(certfile, &pem.Block{
-        Type:  "CERTIFICATE",
-        Bytes: dercert})
-    certfile.Close()
+	path := fmt.Sprintf("%s%c%s.crt", bumper.certdir, os.PathSeparator, name)
+	certfile, err := os.Create(path)
+	if err != nil {
+		return nil, errors.New(
+			fmt.Sprintf("Failed to save certificate as %s", path))
+	}
+	pem.Encode(certfile, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: dercert})
+	certfile.Close()
 
-    path = fmt.Sprintf("%s%c%s.key", bumper.certdir, os.PathSeparator, name)
-    keyfile, err := os.Create(path)
-    if err != nil {
-        return nil, errors.New(
-            fmt.Sprintf("Failed to save private key as %s", path))
-    }
-    pem.Encode(keyfile, &pem.Block{
-        Type:  "RSA PRIVATE KEY",
-        Bytes: privkey,
-    })
-    keyfile.Close()
+	path = fmt.Sprintf("%s%c%s.key", bumper.certdir, os.PathSeparator, name)
+	keyfile, err := os.Create(path)
+	if err != nil {
+		return nil, errors.New(
+			fmt.Sprintf("Failed to save private key as %s", path))
+	}
+	pem.Encode(keyfile, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privkey,
+	})
+	keyfile.Close()
 
-    log.Printf("Created certificate for %s\n", name)
+	log.Printf("Created certificate for %s\n", name)
 
-    bumper.mutex.Lock()
-    bumper.certs[name] = cert
-    bumper.mutex.Unlock()
+	bumper.mutex.Lock()
+	bumper.certs[name] = cert
+	bumper.mutex.Unlock()
 
-    return cert, nil
+	return cert, nil
 }
 
 func isCertificateValid(ca, cert *tls.Certificate, name string) (valid bool) {
-    // Make sure certificate is still valid.
-    pool := x509.NewCertPool()
-    pool.AddCert(ca.Leaf)
+	// Make sure certificate is still valid.
+	pool := x509.NewCertPool()
+	pool.AddCert(ca.Leaf)
 
-    _, err := cert.Leaf.Verify(x509.VerifyOptions{
-        DNSName: name,
-        Roots:   pool,
-    })
+	_, err := cert.Leaf.Verify(x509.VerifyOptions{
+		DNSName: name,
+		Roots:   pool,
+	})
 
-    if err == nil {
-        return true
-    } else {
-        return false
-    }
+	if err == nil {
+		return true
+	} else {
+		return false
+	}
 }
 
 func ReadCertificates(dir string,
-    cacert *tls.Certificate,
-    certs map[string]*tls.Certificate,
-    maxserial *int64) (err error) {
-    // Open directory. If it does not exist, try to create it.
-    directory, err := os.Open(dir)
-    if err != nil {
-        err = os.Mkdir(dir, 0755)
-        if err != nil {
-            log.Printf("Error opening certificate directory %s: %s\n", dir,
-                err)
-            return err
-        }
+	cacert *tls.Certificate,
+	certs map[string]*tls.Certificate,
+	maxserial *int64) (err error) {
+	// Open directory. If it does not exist, try to create it.
+	directory, err := os.Open(dir)
+	if err != nil {
+		err = os.Mkdir(dir, 0755)
+		if err != nil {
+			log.Printf("Error opening certificate directory %s: %s\n", dir,
+				err)
+			return err
+		}
 
-        directory, err = os.Open(dir)
-        if err != nil {
-            log.Printf("Error opening certificate directory %s: %s\n", dir,
-                err)
-            return err
-        }
-    }
-    defer directory.Close()
+		directory, err = os.Open(dir)
+		if err != nil {
+			log.Printf("Error opening certificate directory %s: %s\n", dir,
+				err)
+			return err
+		}
+	}
+	defer directory.Close()
 
-    *maxserial = 1 // 1 is the CA certificate
+	*maxserial = 1 // 1 is the CA certificate
 
-    files, err := directory.Readdir(0)
-    if err != nil {
-        log.Printf("Error opening certificate directory %s: %s\n", dir, err)
-        return err
-    }
+	files, err := directory.Readdir(0)
+	if err != nil {
+		log.Printf("Error opening certificate directory %s: %s\n", dir, err)
+		return err
+	}
 
-    // Loop through the directory, trying to read in every certificate/key.
-    // Certificate filenames must have the ".crt" suffix, and their
-    // corresponding private key must have the ".key" suffix. Subdirectories
-    // are not traversed.
-    for i := range files {
-        file := files[i]
-        if file.IsDir() || !strings.HasSuffix(file.Name(), ".crt") {
-            continue
-        }
+	// Loop through the directory, trying to read in every certificate/key.
+	// Certificate filenames must have the ".crt" suffix, and their
+	// corresponding private key must have the ".key" suffix. Subdirectories
+	// are not traversed.
+	for i := range files {
+		file := files[i]
+		if file.IsDir() || !strings.HasSuffix(file.Name(), ".crt") {
+			continue
+		}
 
-        certpath := fmt.Sprintf("%s%c%s", dir, os.PathSeparator, file.Name())
-        keypath := fmt.Sprintf("%s%c%s", dir, os.PathSeparator,
-            strings.TrimSuffix(file.Name(), ".crt")+".key")
+		certpath := fmt.Sprintf("%s%c%s", dir, os.PathSeparator, file.Name())
+		keypath := fmt.Sprintf("%s%c%s", dir, os.PathSeparator,
+			strings.TrimSuffix(file.Name(), ".crt")+".key")
 
-        cert, err := ReadCert(certpath, keypath)
-        if err != nil {
-            log.Printf("Skipping %s: can't parse cert\n", file.Name())
-            continue
-        }
-        leaf := cert.Leaf
+		cert, err := ReadCert(certpath, keypath)
+		if err != nil {
+			log.Printf("Skipping %s: can't parse cert\n", file.Name())
+			continue
+		}
+		leaf := cert.Leaf
 
-        if leaf.DNSNames != nil {
-            for _, name := range leaf.DNSNames {
-                if !isCertificateValid(cacert, cert, name) {
-                    log.Printf("Invalid cert for %s, removing\n", name)
-                    os.Remove(certpath)
-                    os.Remove(keypath)
-                    continue
-                }
+		if leaf.DNSNames != nil {
+			for _, name := range leaf.DNSNames {
+				if !isCertificateValid(cacert, cert, name) {
+					log.Printf("Invalid cert for %s, removing\n", name)
+					os.Remove(certpath)
+					os.Remove(keypath)
+					continue
+				}
 
-                log.Printf("Found certificate for %s (#%d)\n", name,
-                    leaf.Subject.CommonName, leaf.SerialNumber)
-                certs[name] = cert
-                if leaf.SerialNumber.Int64() > *maxserial {
-                    *maxserial = leaf.SerialNumber.Int64()
-                }
-            }
-        } else if leaf.Subject.CommonName != "" {
-            log.Printf("Found certificate for %s (#%d)\n",
-                leaf.Subject.CommonName, leaf.SerialNumber)
-            certs[string(leaf.Subject.CommonName)] = cert
-            if leaf.SerialNumber.Int64() > *maxserial {
-                *maxserial = leaf.SerialNumber.Int64()
-            }
-        }
-    }
+				log.Printf("Found certificate for %s (#%d)\n", name,
+					leaf.Subject.CommonName, leaf.SerialNumber)
+				certs[name] = cert
+				if leaf.SerialNumber.Int64() > *maxserial {
+					*maxserial = leaf.SerialNumber.Int64()
+				}
+			}
+		} else if leaf.Subject.CommonName != "" {
+			log.Printf("Found certificate for %s (#%d)\n",
+				leaf.Subject.CommonName, leaf.SerialNumber)
+			certs[string(leaf.Subject.CommonName)] = cert
+			if leaf.SerialNumber.Int64() > *maxserial {
+				*maxserial = leaf.SerialNumber.Int64()
+			}
+		}
+	}
 
-    return nil
+	return nil
 }
 
 func ReadCert(certpath, keypath string) (cert *tls.Certificate,
-    err error) {
-    crt, err := tls.LoadX509KeyPair(certpath, keypath)
-    if err != nil {
-        return nil, errors.New(fmt.Sprintf(
-            "Error loading certificate from %s; %s", certpath, keypath))
-    }
+	err error) {
+	crt, err := tls.LoadX509KeyPair(certpath, keypath)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf(
+			"Error loading certificate from %s; %s", certpath, keypath))
+	}
 
-    leaf, err := x509.ParseCertificate(crt.Certificate[0])
-    if err != nil {
-        return nil, errors.New(fmt.Sprintf(
-            "Error parsing certificate from %s; %s", certpath, keypath))
-    }
-    crt.Leaf = leaf
+	leaf, err := x509.ParseCertificate(crt.Certificate[0])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf(
+			"Error parsing certificate from %s; %s", certpath, keypath))
+	}
+	crt.Leaf = leaf
 
-    return &crt, nil
+	return &crt, nil
 }
