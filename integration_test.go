149f317c919dd63675c051e23c0830306c473828
Vilmos Nebehaj <v.nebehaj@gmail.com>
2014-10-24 00:07:47 +0200

    Add websocket test.

--- integration_test.go
+++ integration_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"code.google.com/p/go.net/websocket"
 	"crypto/tls"
 	"github.com/elazarl/goproxy"
 	"io"
@@ -14,6 +16,10 @@ import (
 
 func handler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello world!\n")
+}
+
+func wsEchoHandler(ws *websocket.Conn) {
+	io.Copy(ws, ws)
 }
 
 func createConfig(certfile string, keyfile string, parent string) (
@@ -194,4 +200,68 @@ func TestHttpsParentproxy(t *testing.T) {
 	defer listener.Close()
 
 	doTestHttps(t, parent)
+}
+
+func TestWsViaParentproxy(t *testing.T) {
+	log.SetOutput(noLog{})
+
+	wsaddr := "localhost:12343"
+	wslistener, err := net.Listen("tcp", wsaddr)
+	go func() {
+		http.Serve(wslistener, websocket.Handler(wsEchoHandler))
+	}()
+	defer wslistener.Close()
+
+	parentaddr := "localhost:12344"
+	listener, err := startParentProxy(parentaddr)
+	if err != nil {
+		t.Error("Listen: ", err)
+	}
+	defer listener.Close()
+
+	bumperaddr := "localhost:12346"
+	bumperlistener := startBumper(t, bumperaddr, parentaddr)
+	defer bumperlistener.Close()
+
+	conn, err := net.Dial("tcp", bumperaddr)
+	if err != nil {
+		t.Error("Dial: ", err)
+	}
+	defer conn.Close()
+
+	conn.Write([]byte("CONNECT " + wsaddr + " HTTP/1.1\r\n" +
+		"Host: " + wsaddr + "\r\n\r\n"))
+	req, err := http.NewRequest("CONNECT", wsaddr, nil)
+	if err != nil {
+		t.Error("CONNECT: ", err)
+	}
+	resp, err := http.ReadResponse(bufio.NewReader(conn), req)
+	if err != nil {
+		t.Error("CONNECT: ", err)
+	} else if resp.StatusCode != 200 {
+		t.Error("CONNECT failed, status code: ", resp.StatusCode)
+	}
+
+	config, err := websocket.NewConfig("ws://"+wsaddr, "http://localhost")
+	if err != nil {
+		t.Error("Creating websocket config: ", err)
+	}
+	ws, err := websocket.NewClient(config, io.ReadWriteCloser(conn))
+	if err != nil {
+		t.Error("Creating websocket client: ", err)
+	}
+
+	hello := "Hello world!\n"
+	if _, err := ws.Write([]byte(hello)); err != nil {
+		t.Error("Sending websocket message: ", err)
+	}
+	reply := make([]byte, 512)
+	var n int
+	if n, err = ws.Read(reply); err != nil {
+		t.Error("Receiving websocket message: ", err)
+	}
+
+	if n != len(hello) || string(reply[:n]) != hello {
+		t.Error("Received invalid websocket reply: ", reply)
+	}
 }
